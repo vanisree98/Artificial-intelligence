ca5e7a3aa14cdd40577d964c8286599cf39d91c4
Vanisree <93470123+vanisree98@users.noreply.github.com>
2023-01-02 21:17:22 +0530

    Add files via upload

---
+++ Label reading using Optical Character recognition/opticalCharacterRecognition.py
@@ -0,0 +1,22 @@
+#pillow image read library
+try:
+    from PIL import Image
+except ImportError:
+    import Image
+
+#OCR
+import pytesseract
+
+#Exe. File 
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+def recText(filename):
+    text = pytesseract.image_to_string(Image.open(filename))
+    return text
+
+info = recText('TEST2.png')
+print(info)
+file = open("result.txt","w")
+file.write(info)
+file.close()
+print("Written Successful")
